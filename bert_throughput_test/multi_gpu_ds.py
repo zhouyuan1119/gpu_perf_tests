e100d972857ee364f9d86e09a780ad2c2c2cc56c
Yuan Zhou <yz882@cornell.edu>
2022-12-07 16:03:27 +0000

    [BertThroughputTest] Now allow creating multiple layers. Slight refactoring.

--- bert_throughput_test/multi_gpu_ds.py
+++ bert_throughput_test/multi_gpu_ds.py
@@ -8,58 +8,54 @@ import time
 import torch
 import deepspeed.comm as dist
 
-from utils import build_layer, rough_flops, write_csv
+from utils import build_layers, rough_flops, write_csv, get_parser
 
 def parse_args():
-    parser = argparse.ArgumentParser()
-    parser.add_argument('--bs', type=int, required=True, help='Batch size. ')
-    parser.add_argument('--hs', type=int, required=True, help='Hidden size. ')
-    parser.add_argument('--num_heads', type=int, required=True, help='Number of attention heads. ')
-    parser.add_argument('--seq_len', type=int, required=True, help='Sequence length. ')
-    parser.add_argument('--csv_file', type=str, default='stats.csv', help='Path to the CSV file for storing results. ')
+    parser = get_parser()
     parser = deepspeed.add_config_arguments(parser)
 
     args, _ = parser.parse_known_args()
     return args
 
-def train_multi_gpu_ds(args, layer_engine: deepspeed.DeepSpeedEngine, warmup=5, n_batches=10):
-    if layer_engine.local_rank == 0:
+def train_multi_gpu_ds(args, model_engine: deepspeed.DeepSpeedEngine, warmup=5, n_batches=10):
+    if model_engine.local_rank == 0:
         print('Transferring input and label to GPU... ')
     inputs = torch.rand((args.bs, args.seq_len, args.hs), dtype=torch.float16)
-    inputs = inputs.to(layer_engine.local_rank)
+    inputs = inputs.to(model_engine.local_rank)
     labels = torch.rand((args.bs, args.seq_len, args.hs), dtype=torch.float16)
-    labels = labels.to(layer_engine.local_rank)
-    if layer_engine.local_rank == 0:
+    labels = labels.to(model_engine.local_rank)
+    if model_engine.local_rank == 0:
         print('Warming up... ')
     for i in range(warmup):
-        outputs = layer_engine(inputs)
+        outputs = model_engine(inputs)
         loss = (labels - outputs[0]).sum()
-        layer_engine.backward(loss)
-        layer_engine.step()
+        model_engine.backward(loss)
+        model_engine.step()
 
-    if layer_engine.local_rank == 0:
+    if model_engine.local_rank == 0:
         print('Running... ')
     start = time.time()
     for i in range(n_batches):
-        outputs = layer_engine(inputs)
+        outputs = model_engine(inputs)
         loss = (labels - outputs[0]).sum()
-        layer_engine.backward(loss)
-        layer_engine.step()
+        model_engine.backward(loss)
+        model_engine.step()
     end = time.time()
     dist.log_summary()
 
-    if layer_engine.local_rank == 0:
+    if model_engine.local_rank == 0:
         print('Done!')
-    if layer_engine.local_rank == 0:
+    if model_engine.local_rank == 0:
         print('Max memory allocated in MBs: ', torch.cuda.max_memory_allocated() / (1024*1024))
         print('Total time in seconds: ', end - start)
     avg = (end - start) / n_batches
-    rough_fwd_flops, rough_bwd_flops = rough_flops(args.hs, args.seq_len)
+    rough_fwd_flops, rough_bwd_flops = rough_flops(args.hs, args.seq_len, args.num_layers)
     res_dict = {
         'batch_size': args.bs,
         'seq_len': args.seq_len,
         'hidden_size': args.hs,
         'num_heads': args.num_heads,
+        'num_layers': args.num_layers,
         'avg_time_ms': avg * 1e3,
         'avg_TFLOPS': (rough_bwd_flops + rough_fwd_flops) * args.bs / (avg * 1e12) 
     }
@@ -75,12 +71,12 @@ if __name__ == '__main__':
     deepspeed.init_distributed(dist_backend='nccl')
     args.local_rank = int(os.environ['LOCAL_RANK'])
 
-    layer = build_layer(args)
-    trainable_params = filter(lambda p: p.requires_grad, layer.parameters())
+    layers = build_layers(args)
+    trainable_params = filter(lambda p: p.requires_grad, layers.parameters())
 
     model_engine, _, _, _ = deepspeed.initialize(
         args=args, 
-        model=layer,
+        model=layers,
         model_parameters=trainable_params,
     )
 
--- bert_throughput_test/single_gpu.py
+++ bert_throughput_test/single_gpu.py
@@ -2,15 +2,10 @@ import argparse
 import time
 import torch
 import torch.nn as nn
-from utils import rough_flops, build_layer, write_csv
+from utils import rough_flops, build_layers, write_csv, get_parser
 
 def parse_args():
-    parser = argparse.ArgumentParser()
-    parser.add_argument('--bs', type=int, required=True, help='Batch size. ')
-    parser.add_argument('--hs', type=int, required=True, help='Hidden size. ')
-    parser.add_argument('--num_heads', type=int, required=True, help='Number of attention heads. ')
-    parser.add_argument('--seq_len', type=int, required=True, help='Sequence length. ')
-    parser.add_argument('--csv_file', type=str, default='stats.csv', help='Path to the CSV file for storing results. ')
+    parser = get_parser()
     args, _ = parser.parse_known_args()
     return args
 
@@ -39,12 +34,13 @@ def train_single_gpu(args, layer: nn.Module, warmup=5, n_batches=100):
     print('Max memory allocated in MBs: ', torch.cuda.max_memory_allocated() / (1024*1024))
     print('Total time in seconds: ', end - start)
     avg = (end - start) / n_batches
-    rough_fwd_flops, rough_bwd_flops = rough_flops(args.hs, args.seq_len)
+    rough_fwd_flops, rough_bwd_flops = rough_flops(args.hs, args.seq_len, args.num_layers)
     res_dict = {
         'batch_size': args.bs,
         'seq_len': args.seq_len,
         'hidden_size': args.hs,
         'num_heads': args.num_heads,
+        'num_layers': args.num_layers,
         'avg_time_ms': avg * 1e3,
         'avg_TFLOPS': (rough_bwd_flops + rough_fwd_flops) * args.bs / (avg * 1e12) 
     }
@@ -52,6 +48,6 @@ def train_single_gpu(args, layer: nn.Module, warmup=5, n_batches=100):
 
 if __name__ == '__main__':
     args = parse_args()
-    layer = build_layer(args)
+    layer = build_layers(args)
     res_dict = train_single_gpu(args, layer)
     write_csv(res_dict, args.csv_file)
